a8dd600bfd2ca98c624fe4cfaacaa47359fc0523
Avik-creator <avikm744@gmail.com>
2024-12-25 19:09:49 +0530

    Added Changes.

--- src/components/form-generator/formGenerator.tsx
+++ src/components/form-generator/formGenerator.tsx
@@ -47,7 +47,7 @@ export default function FormGenerator() {
         });
       }
     } catch (error) {
-
+      console.error("Error generating form:", error);
     } finally {
       setIsGenerating(false);
     }
