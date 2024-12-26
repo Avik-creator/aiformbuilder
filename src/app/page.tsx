434bedcbe7ff8b3c81a3bcc8d5d6a6ffb8c89e1a
Avik-creator <avikm744@gmail.com>
2024-12-26 21:00:12 +0530

    Added Changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ import Header from "@/components/header";
 import CTA from "@/components/homepage/cta";
 import Features from "@/components/homepage/features";
 import Hero from "@/components/homepage/hero";
+import type { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: "AI Form Generator",
+  description: "It's a form generator that uses AI to generate google forms.",
+  verification: {
+    google: process.env.GOOGLE_VERIFICATION_ID
+  }
+};
 
 export default function Home() {
   return (
