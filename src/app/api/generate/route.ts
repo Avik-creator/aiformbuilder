45123048d7011c90354f6fd6afd2f5de6646161f
Avik-creator <avikm744@gmail.com>
2024-12-26 17:30:12 +0530

    Added MaxDuration.

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,7 +1,5 @@
 "use server";
 
-export const maxDuration = 60; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
 
 import { revalidatePath } from "next/cache";
 
