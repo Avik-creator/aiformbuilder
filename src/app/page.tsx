e31260f0abc8772e5bb24858a456b98c826d234e
Avik-creator <avikm744@gmail.com>
2025-03-21 10:19:05 +0530

    Added Changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,30 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "AI Form Generator",
-  description: "It's a form generator that uses AI to generate google forms.",
   verification: {
     google: process.env.GOOGLE_VERIFICATION_ID
+  },
+  description: "Want to create a form? FormCraft AI is here to help you generate forms with AI.",
+  abstract: "Want to create a form? FormCraft AI is here to help you generate forms with AI.",
+  creator: "Avik Mukherjee",
+   metadataBase: new URL("https://formcraftai-delta.vercel.app"),
+  openGraph:{
+    title: "FormCraft AI",
+    description: "Want to create a form? FormCraft AI is here to help you generate forms with AI.",
+    type: "website",
+    locale: "en_US",
+    siteName: "FormCraft AI",
+    countryName:"India",
+    url:"https://formcraftai-delta.vercel.app"
+  },
+  twitter:{
+    card: "summary",
+    creator: "Avik Mukherjee",
+    site: "FormCraft AI",
+    title: "FormCraft AI",
+    description: "Want to create a form? FormCraft AI is here to help you generate forms with AI.",
   }
+  
 };
 
 export default function Home() {
